f8beb21b65abd15806d457ab62ef5ab42a18e07c
Rifki Harbi Awali <rifqiawali14@gmail.com>
2020-10-09 23:06:18 +0700

    cleaning main

--- src/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-
-import thunk from "redux-thunk";
-import AllRed from "../reducer";
-
-const store = createStore(AllRed, applyMiddleware(thunk));
-
-export default store;
